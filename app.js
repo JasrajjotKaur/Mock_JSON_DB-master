c8ad7af064b1fc0e10c579aaf1d483aea5652a96
HeyJasraj12 <bejasrajjotkaur@gmail.com>
2025-04-01 11:22:59 -0600

    Update

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const express = require('express');
 require('dotenv').config();
 const PORT = process.env.PORT;
-const userRouter = require('./routes/users.js')
+const userRoutes = require('./routes/users.js');
 const app = express();
+const rateLimit = require('express-rate-limit');
 
 //setup our view engine
 app.set('view engine', 'ejs');
 app.set('views', "./views");
 
+//rate limiter
+const fixedWindowLimiter = rateLimit({
+ windowMs: 1 * 15 * 1000,
+ max: 10,
+ message: 'Too many requests.  Please try again later',
+});
+
+app.use(fixedWindowLimiter);
+
 
 app.use(express.static('public'));
 app.use(userRoutes);
+
 
 app.listen(PORT, ()=>{
     console.log(`Connected to port: ${PORT}`);
--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const userController = require('../controllers/userController.js');
 
 
 
-
-// render our home page
+//render our home page
 router.get('/', (req, res)=>{
     res.render('home');
 });
 
+//post route handler to create a new user
+router.post('/users', userController.createUser);
 
 
-
-module.export = router;
+module.exports = router;
